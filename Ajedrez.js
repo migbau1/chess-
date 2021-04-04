bcef9a296120d88a4a5268953759091e470b181e
Miguel Bautista <miguelangelbautistasilva2@gmail.com>
2021-04-03 22:44:07 -0500

    add all pieces in board and succes movement smooth

--- Ajedrez.js
+++ Ajedrez.js
@@ -20,7 +20,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const board = ctx.fillRect(0, 0, lienzoWidth, lienzoHeigth);
 
   drawSquares();
-  putPieces();
+  createPieces();
+  printPieces();
+
   lienzo.onmousedown = mouseDown;
   lienzo.onmouseup = mouseUp;
   lienzo.onmousemove = mouseMove;
@@ -35,7 +37,7 @@ class Square {
     x = 0,
     y = 0,
     name,
-    color = Object.freeze({ white: "#ffffff", black: "#000000" }),
+    color = Object.freeze({ white: "#ffffff", black: "#3d4fbd" }),
     draggable = false
   ) {
     this.x = x;
@@ -44,7 +46,7 @@ class Square {
     this.w = lienzoHeigth / 8;
     this.h = lienzoHeigth / 8;
 
-    this.c = color;
+    this.c = Object.freeze({ white: "#ffffff", black: "#3d4fbd" });
     this.name = name;
 
     this.draggable = draggable;
@@ -52,12 +54,12 @@ class Square {
 }
 
 class Piece {
-  constructor(name, x, y, color, pic, draggable = false) {
+  constructor(name, x, y, color, draggable = false) {
     this.name = name;
     this.x = x;
     this.y = y;
     this.color = color;
-    this.pic = pic;
+    this.pic = new Image();
     this.draggable = draggable;
     this.w = lienzoHeigth / 8;
     this.h = lienzoHeigth / 8;
@@ -96,28 +98,112 @@ function draw(x, y, color, name) {
   let squ = new Square(x, y, name, color);
   historySquares.push(squ);
 
-  ctx.fillStyle = squ.c;
+  ctx.fillStyle = squ.c[`${color}`];
 
   ctx.fillRect(squ.x, squ.y, squ.w, squ.h);
 }
 
-function putPieces() {
-  let piece = new Piece("torre", 0, 437.5, "white", "./assets/img/wr.svg");
-  createPiece(piece.x, piece.y, piece.w, piece.h, piece.pic);
+function createPieces() {
+  let limit = lienzoHeigth / 8;
+
+  //white pieces
+  let wrl = new Piece("wrl", 0, lienzoWidth - limit, "white");
+  wrl.pic.src = "./assets/img/wr.svg";
+
+  let wnl = new Piece("wnl", limit, lienzoWidth - limit, "white");
+  wnl.pic.src = "./assets/img/wn.svg";
+
+  let wbl = new Piece("wbl", limit * 2, lienzoWidth - limit, "white");
+  wbl.pic.src = "./assets/img/wb.svg";
+
+  let wq = new Piece("wq", limit * 3, lienzoWidth - limit, "white");
+  wq.pic.src = "./assets/img/wq.svg";
+
+  let wk = new Piece("wk", limit * 4, lienzoWidth - limit, "white");
+  wk.pic.src = "./assets/img/wk.svg";
+
+  let wbr = new Piece("wbr", limit * 5, lienzoWidth - limit, "white");
+  wbr.pic.src = "./assets/img/wb.svg";
+
+  let wnr = new Piece("wnr", limit * 6, lienzoWidth - limit, "white");
+  wnr.pic.src = "./assets/img/wn.svg";
+
+  let wrr = new Piece("wrr", limit * 7, lienzoWidth - limit, "white");
+  wrr.pic.src = "./assets/img/wr.svg";
+
+  for (i = 0; i < 8; i++) {
+    let wp = new Piece(`wp${i}`, limit * i, lienzoWidth - limit * 2, "white");
+    wp.pic.src = "./assets/img/wp.svg";
+    historyPieces.push(wp);
+  }
+
+  //black pieces
+  let brl = new Piece("brl", 0, 0, "black");
+  brl.pic.src = "./assets/img/br.svg";
+
+  let bnl = new Piece("bnl", limit, 0, "black");
+  bnl.pic.src = "./assets/img/bn.svg";
+
+  let bbl = new Piece("bbl", limit * 2, 0, "black");
+  bbl.pic.src = "./assets/img/bb.svg";
+
+  let bq = new Piece("bq", limit * 3, 0, "black");
+  bq.pic.src = "./assets/img/bq.svg";
+
+  let bk = new Piece("bk", limit * 4, 0, "black");
+  bk.pic.src = "./assets/img/bk.svg";
+
+  let bbr = new Piece("bbr", limit * 5, 0, "black");
+  bbr.pic.src = "./assets/img/bb.svg";
+
+  let bnr = new Piece("bnr", limit * 6, 0, "black");
+  bnr.pic.src = "./assets/img/bn.svg";
+
+  let brr = new Piece("brr", limit * 7, 0, "black");
+  brr.pic.src = "./assets/img/br.svg";
+
+  for (i = 0; i < 8; i++) {
+    let bp = new Piece(`bp${i}`, limit * i, limit, "black");
+    bp.pic.src = "./assets/img/bp.svg";
+    historyPieces.push(bp);
+  }
+
+  historyPieces.push(
+    wrl,
+    wnl,
+    wbl,
+    wq,
+    wk,
+    wbr,
+    wnr,
+    wrr,
+    brl,
+    bnl,
+    bbl,
+    bq,
+    bk,
+    bbr,
+    bnr,
+    brr
+  );
 }
 
-function createPiece(x, y, w, h, pic) {
+function printPieces() {
+  ctx2.clearRect(0, 0, lienzoWidth, lienzoHeigth);
+  // ctx2.fillStyle = "rgba(255, 255, 255, 0)";
+  // ctx2.rect(0, 0, 500, 500);
 
-  let img = new Image();
-  img.onload = () => {
-    clear(0, 0, w, h)
-    ctx2.drawImage(img, x, y, w, h);
-  };
-  img.src = pic;
-}
-
-function clear(x, y, w, h) {
-  ctx2.clearRect(x, y, boardPieces.width, boardPieces.height);
+  for (let i = 0; i < historyPieces.length; i++) {
+    const element = historyPieces[i];
+    let isLoaded = element.pic.complete && Image.naturalHeight !== 0;
+    if (!isLoaded) {
+      element.pic.onload = () => {
+        ctx2.drawImage(element.pic, element.x, element.y, element.w, element.h);
+      };
+    } else {
+      ctx2.drawImage(element.pic, element.x, element.y, element.w, element.h);
+    }
+  }
 }
 
 //this function in in charge of display info to square selected recursively
@@ -167,14 +253,26 @@ function infoSquareSelected(array, itemX, itemY) {
   }
 }
 
+//this function verified if square is available
+function usedSquared(square, listPieces) {
+  let found = false;
+  let position = -1;
+  let index = 0;
+
+  while (!found && index < listPieces.length) {
+    if (listPieces[index].x === square.x && listPieces[index].y === square.y) {
+      found = true;
+      position = index;
+    }
+    index++;
+  }
+  return position === -1 ? undefined : listPieces[position];
+}
+
 function mouseDown(e) {
   // tell the browser we're handling this mouse event
   e.preventDefault();
   e.stopPropagation();
-
-  let limit = 8;
-  let heightMax = lienzoWidth / limit;
-  let sizeMax = lienzoHeigth - heightMax;
 
   let rx = e.clientX - rect.left;
   let ry = e.clientY - rect.top;
@@ -190,17 +288,34 @@ function mouseDown(e) {
   });
 
   let value = infoSquareSelected(orderArray, rx, ry);
-  dragOk = true;
+
+  const orderListPieces = historyPieces.sort((a, b) => {
+    if (a.y > b.y) {
+      return 1;
+    }
+    if (a.y < b.y) {
+      return -1;
+    }
+    return 0;
+  });
+
+  let response = usedSquared(value, orderListPieces);
+
+  dragOk = false;
+  if (response !== undefined) {
+    for (let i = 0; i < historyPieces.length; i++) {
+      const element = historyPieces[i];
+      if (element.x === response.x && element.y === response.y) {
+        dragOk = true;
+        element.draggable = true;
+      }
+    }
+    positionX = rx;
+    postionY = ry;
+  }
 }
 
 function mouseUp(e) {
-  // tell the browser we're handling this mouse event
-  e.preventDefault();
-  e.stopPropagation();
-  dragOk = false;
-}
-
-function mouseMove(e) {
   // tell the browser we're handling this mouse event
   e.preventDefault();
   e.stopPropagation();
@@ -208,8 +323,54 @@ function mouseMove(e) {
   let rx = e.clientX - rect.left;
   let ry = e.clientY - rect.top;
 
-  
+  const orderArray = historySquares.sort((a, b) => {
+    if (a.y > b.y) {
+      return 1;
+    }
+    if (a.y < b.y) {
+      return -1;
+    }
+    return 0;
+  });
+
+  let prox = infoSquareSelected(orderArray, rx, ry);
+
+  dragOk = false;
+  for (let i = 0; i < historyPieces.length; i++) {
+    const element = historyPieces[i];
+    if (element.draggable) {
+      element.x = prox.x;
+      element.y = prox.y;
+    }
+    element.draggable = false;
+  }
+  printPieces()
+}
+
+function mouseMove(e) {
+  // tell the browser we're handling this mouse event
+
   if (dragOk) {
-    createPiece(rx - (62.5 / 2), ry - (62.5 /2), 62.5, 62.5, "./assets/img/wr.svg");
+    e.preventDefault();
+    e.stopPropagation();
+
+    let rx = e.clientX - rect.left;
+    let ry = e.clientY - rect.top;
+
+    let dx = rx - positionX;
+    let dy = ry - postionY;
+
+    for (let i = 0; i < historyPieces.length; i++) {
+      const element = historyPieces[i];
+      if (element.draggable) {
+        element.x += dx;
+        element.y += dy;
+      }
+    }
+
+    printPieces();
+
+    positionX = rx;
+    postionY = ry;
   }
 }
