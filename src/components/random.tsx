a1cc762d023e8451b22b914b8b1121e127c435e0
miguel angel bautista <miguelangelbautistasilva2@gmail.com>
2023-03-14 16:37:06 -0500

    first config

---
+++ src/components/random.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function RandomComponent(): JSX.Element {
+    return (<div>
+        this is my component
+    </div>)
+}
+
+export default RandomComponent;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@', replacement: './src'
+      },
+      {
+        find: '@components', replacement: './src/components'
+      }
+    ]
+  },
   plugins: [react()],
 })
